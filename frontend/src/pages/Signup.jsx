6880eed74074522d4f75827f23b62d57b479d369
Wasi <wbhuyian201286@bscse.uiu.ac.bd>
2024-04-03 01:00:24 +0600

    Deployment Starts

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import MainLayout from '../layouts/MainLayout'
-import SignupForm from '../components/SignupForm';
+import SignupForm from '../components/SignupForm'
 
 const Signup = () => {
 
