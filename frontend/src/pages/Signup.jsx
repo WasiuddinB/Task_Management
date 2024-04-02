7a51d4242bbbae92fff521d5f798d65d7ce09862
Wasi <wbhuyian201286@bscse.uiu.ac.bd>
2024-04-02 15:57:41 +0600

    Login Page, Sign Up Page

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react'
+import MainLayout from '../layouts/MainLayout'
+
+
+const Signup = () => {
+
+  useEffect(() => {
+    document.title = "Signup";
+  }, []);
+  return (
+    <>
+      <MainLayout>
+        
+      </MainLayout>
+    </>
+  )
+}
+
+export default Signup
